6669f74753630f8e2e445d9be19c3120cdca5ba2
Louis Yu <louis29418401@gmail.com>
2013-04-30 22:10:44 +0800

    completed version

--- js/louis.js
+++ js/louis.js
@@ -5,9 +5,24 @@ function init(){
 		$('#myModal').modal('show');
 	});
 	$("#gco2").mouseover(function(){
-		$("#gco2").popover('show');
+		$(this).popover('show');
 	});
 	$("#gco2").mouseout(function(){
-		$("#gco2").popover('hide');
+		$(this).popover('hide');
 	});
+
+	$("#coal").mouseover(function(){
+		$(this).popover('show');
+	});
+	$("#coal").mouseout(function(){
+		$(this).popover('hide');
+	});
+
+	$("#factory").mouseover(function(){
+		$(this).popover('show');
+	});
+	$("#factory").mouseout(function(){
+		$(this).popover('hide');
+	});
+
 }
